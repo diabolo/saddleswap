aff041ca05904e65216778d214f7c6f2855dd5c0
Andrew Premdas <apremdas@yahoo.co.uk>
2014-08-20 20:46:26 +0100

    first feature, start

---
+++ features/see_saddles.feature
@@ -0,0 +1,9 @@
+Feature: See saddles
+  As someone who wants to try different saddles
+  I want to see what saddles are available
+
+  @wip
+  Scenario: View saddles
+    Given there are some saddles
+    When I view saddles
+    Then I should see some saddles
---
+++ features/step_definitions/saddles_steps.rb
@@ -0,0 +1,13 @@
+
+Given(/^there are some saddles$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+When(/^I view saddles$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see some saddles$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
