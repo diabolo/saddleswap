619a978b8fc28be5b321efa8eae01e5ffde80393
Andrew Premdas <apremdas@yahoo.co.uk>
2014-08-21 10:31:57 +0100

    wip, need saddle migration

---
+++ app/models/saddle.rb
@@ -0,0 +1,2 @@
+class Saddle < ActiveRecord::Base
+end
--- features/step_definitions/saddles_steps.rb
+++ features/step_definitions/saddles_steps.rb
@@ -1,6 +1,27 @@
 
+module SH
+  def some
+    3
+  end
+end
+World SH
+
+module SaddleSH
+  def create_saddle
+    Saddle.create(default_saddle)
+  end
+
+  def default_saddle
+    {
+      manufacturer: 'Fizik',
+      model: 'Arione',
+    }
+    end
+end
+World SaddleSH
+
 Given(/^there are some saddles$/) do
-    pending # express the regexp above with the code you wish you had
+  some.times do create_saddle; end
 end
 
 When(/^I view saddles$/) do
